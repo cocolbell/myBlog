b18be087d4d7f904c1d7f490a0f2167896ea42ae
cocolbell <cocolbell@163.com>
2018-03-26 13:36:19 +0800

    ES6异步加载

--- src/router/router.js
+++ src/router/router.js
@@ -4,19 +4,23 @@
 /**
  * 关于VUE路由懒加载还可以使用AMD规范写法，如下：
  * const archives = resolve => require(['./../view/archives.vue'], resolve)  
- * 但是为了规范化代码，我们采用CommonJS的规范来书写这个异步组件的工厂函数
+ * 采用CommonJS规范如下
+ * const interests = r => require.ensure([], () => r(require('./../view/interests.vue')), 'interests')
+ * const archives = r => require.ensure([], require => require('./../view/archives.vue'), 'archives')
+ * 
+ * 但是为了规范化代码，我们采用ES6的规范来书写这个异步组件的工厂函数
  */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import index from './../view/index.vue'
-const interests = r => require.ensure([], () => r(require('./../view/interests.vue')), 'interests')
-// const archives = r => require.ensure([], require => require('./../view/archives.vue'), 'archives')
-const archives = r => require.ensure([], () => r(require('./../view/archives.vue')), 'archives')
-const login = r => require.ensure([], () => r(require('./../view/login.vue')), 'login')
-const articleEdit = r => require.ensure([], () => r(require('./../view/articleEdit.vue')), 'articleEdit')
-const article = r => require.ensure([], () => r(require('./../view/article.vue')), 'article')
-// const message = r => require.ensure([], () => r(require('./../view/message.vue')), 'message')
+
+const interests = r => System.import(/* webpackChunkName: "interests" */'./../view/interests.vue');
+const archives = r => System.import(/* webpackChunkName: "archives" */'./../view/archives.vue');
+const login = r => System.import(/* webpackChunkName: "login" */'./../view/login.vue');
+const articleEdit = r => System.import(/* webpackChunkName: "articleEdit" */'./../view/articleEdit.vue');
+const article = r => System.import(/* webpackChunkName: "article" */'./../view/article.vue');
 Vue.use(VueRouter)
+
 const router = new VueRouter({
   	routes: [{
     path: '/',
